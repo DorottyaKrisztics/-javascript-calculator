192c7aaf19fa985b22c011883b7439eb9126546e
DorottyaKrisztics <dorka713@gmail.com>
2017-06-11 20:17:43 +0200

    + operation is working for 2 numbers, count function added

--- javascript/script.js
+++ javascript/script.js
@@ -8,4 +8,21 @@ function reset() {
 }
 
 
+function count() {
+    var readFromField = document.getElementById("field").value;
+    var inputLength = readFromField.length;
+    
+    var addPosition = readFromField.indexOf("+");
+    var subPosition = readFromField.indexOf("-");
+    var multiplyPosition = readFromField.indexOf("*");
+    var divPosition = readFromField.indexOf("/");
+    
+    if (addPosition > -1) {
+        var numb1 = readFromField.substring(0, addPosition);       
+        var numb2 = readFromField.slice(addPosition + 1, inputLength);
+        var result = parseFloat(numb1) + parseFloat(numb2);
+        document.getElementById("field").value = result;
+    }   
+}
+
 
